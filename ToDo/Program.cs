eb178f6e69367be697de85624363994d683c738b
FacundoVel <facundovel.99@gmail.com>
2025-04-27 02:24:39 -0300

    Se crea algoritmo para cargar N tareas

--- ToDo/Program.cs
+++ ToDo/Program.cs
@@ -1,12 +1,18 @@
 ﻿using EspacioTarea
 
-int n = 0;
+int n = 0, id = 0, duracion = 0; 
+
 Console.WriteLine("Ingrese cantidad de Tareas a realizar: ");
-if((int.TryParse(Console.ReadLine(),out n)) && n >= 0)
+if((int.TryParse(Console.ReadLine(),out n)) && n > 0)
 {
     for (int i = 0; i < n; i++)
     {
-        tarea.add()
+        int _tareaID = id++;
+        Console.WriteLine("Ingrese la descripcion de la tarea: ");
+        string _descripcion = Console.ReadLine();
+        Console.WriteLine("Ingrese la duracion de la tarea: ");
+        int _duracion = int.Parse(Console.Readline(), out duracion);
+        Tarea.add(_tareaID, _descripcion, _duracion);
     }
 }
 else{
